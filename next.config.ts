d0dcb4189f5d17e3e969338ab51341083985fc66
EMİR KALAYCI <emir@EMIR-MacBook-Pro.local>
2026-04-19 15:48:52 +0300

    Remove eslint ignoreDuringBuilds config

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,7 @@ const nextConfig: NextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
